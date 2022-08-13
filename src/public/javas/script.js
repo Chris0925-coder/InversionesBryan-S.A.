9c1d603084145a8ea4b221868d0b6c41ab1353b9
= <=>
2022-08-13 02:33:23 -0500

    node.js

---
+++ src/public/javas/script.js
@@ -0,0 +1,24 @@
+let navToggle = document.querySelector(".navbar-toggler");
+let navLink = document.querySelector(".navbar-nav");
+
+navToggle.addEventListener("click", () => {
+    navLink.classList.toggle("navbar-nav_visible")
+
+});
+
+// let comment = document.querySelector(".btn"); 
+// let ff = document.querySelector("form-control"); 
+
+
+// comment.addEventListener("click", send);
+// ff.addEventListener(Function, send);
+// 	function send() {
+// 		ff.addEventListener()
+// 	if (ff === String) {
+// 	alert("¡Gracias!, su comentario ha sido enviado.")
+// }
+// 	else {
+// 		alert("Rellene las casillas en blanco")
+// 	}
+// };	
+
