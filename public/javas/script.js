536781ce997044af0b3dd8528ac0e3a34bf85d05
= <=>
2022-08-14 15:05:04 -0500

    restoredPage

---
+++ public/javas/script.js
@@ -0,0 +1,31 @@
+let navToggle = document.querySelector(".navbar-toggler");
+let navLink = document.querySelector(".navbar-nav");
+
+navToggle.addEventListener("click", () => {
+    navLink.classList.toggle("navbar-nav_visible")
+
+});
+
+// let comment = document.querySelector(".btn"); 
+// let ff = document.querySelector("form-control"); 
+
+
+// comment.addEventListener("click", send);
+// ff.addEventListener(Function, send);
+// 	function send() {
+// 		ff.addEventListener()
+// 	if (ff === String) {
+// 	alert("¡Gracias!, su comentario ha sido enviado.")
+// }
+// 	else {
+// 		alert("Rellene las casillas en blanco")
+// 	}
+// };	
+
+// let nav = document.getElementById("navjava");
+// nav.innerHTML("ind1");
+
+let getImport = document.querySelector('link[rel=import]');
+let getContent = getImport.import.querySelector('#ind1');
+document.body.appendChild(document.importNode(getContent, true));
+
