f6f26169c83a7632c48b4c77672525bafe70846c
= <=>
2022-09-12 02:21:41 -0500

    addpost

--- comment.php
+++ comment.php
@@ -1,6 +1,6 @@
 <?php
-$email = $_GET['email'];
-$control = $_GET['control'];
+$email = $_POST['email'];
+$control = $_POST['control'];
 
 $header = 'From ' . $email . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -8,7 +8,7 @@ $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 $control = "Este mensaje fue desde la pagina web por: " . $email . " \r\n";
-$control .= "Comentario: " . $_GET['comentario'] . " \r\n";
+$control .= "Comentario: " . $_POST['comentario'] . " \r\n";
 $control .= "Con fecha: " . date('d/m/Y', time());
 
 $para = 'cguardia3925@gmail.com';
