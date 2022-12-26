4aeacae68c01540e4f82f121555e2672d8022ad0
= <=>
2022-12-26 15:18:22 -0500

    8.7.5

---
+++ public/javas/cookies.js
@@ -0,0 +1,14 @@
+function compruebaAceptaCookies() {
+    if (localStorage.aceptaCookies == 'true') {
+        cajacookies.style.display = 'none';
+    }
+};
+
+function aceptarCookies() {
+    localStorage.aceptaCookies == 'true';
+        cajacookies.style.display = 'none';
+};
+
+$(document).ready(function() {
+    compruebaAceptaCookies();
+});
