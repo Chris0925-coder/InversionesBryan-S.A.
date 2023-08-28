95836dc570788ea425f405f8ca9831ddbfbc4925
Chris0925-coder <christianguardia@outlook.es>
2023-08-28 02:33:49 -0500

    ib2

--- public/javas/cookies.js
+++ public/javas/cookies.js
@@ -1,3 +1,11 @@
+let navToggle = document.querySelector(".navbar-toggler");
+let navLink = document.querySelector(".navbar-nav");
+
+navToggle.addEventListener("click", () => {
+    navLink.classList.toggle("navbar-nav_visible")
+});
+
+
 function compruebaAceptaCookies() {
     if (localStorage.aceptaCookies == 'true') {
         cajacookies.style.display = 'none';
