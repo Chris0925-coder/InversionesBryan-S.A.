820fd175874caf1bcc9c56674c157549116594bd
Chris0925-coder <christianguardia@outlook.es>
2025-03-11 00:36:49 -0500

    001

--- public/javas/cook.js
+++ public/javas/cook.js
@@ -1,9 +1,3 @@
-let navToggle = document.querySelector(".navbar-toggler");
-let navLink = document.querySelector(".navbar-nav");
-
-navToggle.addEventListener("click", () => {
-    navLink.classList.toggle("navbar-nav_visible")
-});
 
 // const bloqueRGPD = document.querySelector(".cookies");
 const btnCookies = document.querySelector(".btn-cookies")
@@ -123,6 +117,16 @@ function eliminarBloqueRGPD(){
     localStorage.acceptedCookies = 'true';
 };
 
+
+
+let navToggle = document.querySelector(".navbar-toggler");
+let navLink = document.querySelector(".navbar-nav");
+
+navToggle.addEventListener("click", () => {
+    navLink.classList.toggle("navbar-nav_visible")
+
+    console.log('Hi')
+});
 
 
 
